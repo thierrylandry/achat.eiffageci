09136f32ab206bdd9b97bd3f87b2acea7c41cb60
ckodia <cyriaquekodia@gmail.com>
2019-02-27 09:39:11 +0000

    enlever require sur l'email du fournisseur

--- resources/views/fournisseurs/ajouter_fournisseur.blade.php
+++ resources/views/fournisseurs/ajouter_fournisseur.blade.php
@@ -56,7 +56,7 @@
 
                             <div class="form-group">
                                 <label for="email">E - mail</label>
-                                <input type="text" class="form-control" id="email" name="email" placeholder="E -mail" value="{{isset($fournisseur)? $fournisseur->email:''}}" required>
+                                <input type="text" class="form-control" id="email" name="email" placeholder="E -mail" value="{{isset($fournisseur)? $fournisseur->email:''}}">
                             </div>
 
                              </div>
