96e26697d6d89ae36f41d77c6ccc08908127584d
iceberglca <cyriaquekodia@hotmail.fr>
2019-03-18 17:55:51 +0000

    test  test

--- app/Http/Controllers/Demande_proformaController.php
+++ app/Http/Controllers/Demande_proformaController.php
@@ -278,7 +278,7 @@ $i=0;
                 $interlocuteur=$fournisseur->responsable;
             }
         */
-    //    dd("le corp du mail :".$corps." les précision".$precisions." les images".$images);
+        dd($precisions);
 $email=$em;
 if($rappel!="on"){
     Mail::send('mail.mail',array('corps'=>$corps,'precisions'=>$precisions,'images'=>$images),function($message)use ($email,$images ){
