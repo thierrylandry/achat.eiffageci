5f5309cd99202bbd3a636e8ff300204b4e0bfa68
ckodia <cyriaquekodia@gmail.com>
2019-04-12 17:19:35 +0000

    le modele unité

---
+++ app/Unites.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Unites extends Model
+{
+
+    //
+    protected  $table="unites";
+    //
+    protected $fillable= ['id','libelle'];
+}
