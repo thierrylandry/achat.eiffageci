c007cfa6e92f6f737b93d14a62b4f3794196d1ef
ckodia <cyriaquekodia@gmail.com>
2019-05-02 15:13:23 +0000

    test push

--- app/Analytique.php
+++ app/Analytique.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Analytique extends Model
 {
     //
+    
     protected  $table="analytique";
     //
     protected $fillable= ['id_analytique','codeRubrique','libelle'];
