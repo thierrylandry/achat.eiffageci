e6e17441f9001c142f43405dda75d7f7ea4f928b
ckodia <cyriaquekodia@gmail.com>
2019-05-22 17:48:57 +0000

    test(

--- app/Http/Controllers/DAController.php
+++ app/Http/Controllers/DAController.php
@@ -30,7 +30,7 @@ class DAController
     {
         $fournisseurs=Fournisseur::all();
         $materiels=Materiel::all();
-        $das=  DA::orderBy('created_at', 'DESC')->get();
+        $das=  DA::orderBy('created_at', 'DESC')->limit(200)->get();
         $natures= Nature::all();
 
         $service_users=DB::table('users')
@@ -99,7 +99,7 @@ class DAController
     }
     public function voir_da($slug)
     {
-        $das=  DA::where('id_user','=',\Illuminate\Support\Facades\Auth::user()->id)->orderBy('created_at', 'DESC')->get();
+        $das=  DA::where('id_user','=',\Illuminate\Support\Facades\Auth::user()->id)->limit(200)->orderBy('created_at', 'DESC')->get();
         $da = DA::where('slug', '=', $slug)->first();
         $domaines=  DB::table('domaines')->get();
         $fournisseurs=Fournisseur::all();
