22485a3a00faef56d7d0d72d774b6543a7b7b406
ckodia <cyriaquekodia@gmail.com>
2019-06-11 11:29:29 +0000

    amélioration6

--- app/Http/Controllers/DAController.php
+++ app/Http/Controllers/DAController.php
@@ -49,8 +49,8 @@ class DAController
        // $das=  DA::where('id_user','=',\Illuminate\Support\Facades\Auth::user()->id)->orderBy('created_at', 'DESC')->paginate(50);
         $das=  DB::table('lignebesoin')
                 ->Join('users','users.id','=','lignebesoin.id_user')
-                ->where('users.service','=',\Illuminate\Support\Facades\Auth::user()->service)->orderBy('Lignebesoin.created_at', 'DESC')
-            ->select('Lignebesoin.id','unite','quantite','DateBesoin','id_user','id_nature','id_materiel','Lignebesoin.created_at','demandeur','Lignebesoin.slug','etat','id_valideur','motif','usage','commentaire','dateConfirmation','date_livraison_eff')->paginate(50);
+                ->where('users.service','=',\Illuminate\Support\Facades\Auth::user()->service)->orderBy('lignebesoin.created_at', 'DESC')
+            ->select('lignebesoin.id','unite','quantite','DateBesoin','id_user','id_nature','id_materiel','lignebesoin.created_at','demandeur','lignebesoin.slug','etat','id_valideur','motif','usage','commentaire','dateConfirmation','date_livraison_eff')->paginate(50);
         $natures= Nature::all();
         $service_users=DB::table('users')
             ->leftJoin('services', 'services.id', '=', 'users.service')
@@ -106,8 +106,8 @@ class DAController
         //$das=  DA::where('id_user','=',\Illuminate\Support\Facades\Auth::user()->id)->orderBy('created_at', 'DESC')->paginate(50);
         $das=  DB::table('lignebesoin')
             ->Join('users','users.id','=','lignebesoin.id_user')
-            ->where('users.service','=',\Illuminate\Support\Facades\Auth::user()->service)->orderBy('Lignebesoin.created_at', 'DESC')
-            ->select('Lignebesoin.id','unite','quantite','DateBesoin','id_user','id_nature','id_materiel','Lignebesoin.created_at','demandeur','Lignebesoin.slug','etat','id_valideur','motif','usage','commentaire','dateConfirmation','date_livraison_eff')->paginate(50);
+            ->where('users.service','=',\Illuminate\Support\Facades\Auth::user()->service)->orderBy('lignebesoin.created_at', 'DESC')
+            ->select('lignebesoin.id','unite','quantite','DateBesoin','id_user','id_nature','id_materiel','lignebesoin.created_at','demandeur','lignebesoin.slug','etat','id_valideur','motif','usage','commentaire','dateConfirmation','date_livraison_eff')->paginate(50);
 
         $da = DA::where('slug', '=', $slug)->first();
         $domaines=  DB::table('domaines')->get();
