88609339c0a676ad537f68e2550fe3a30885b8a6
ckodia <cyriaquekodia@gmail.com>
2019-08-08 10:24:43 +0000

    test

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -13,6 +13,7 @@ namespace App\Http\Controllers;
 use App\Materiel;
 use Dompdf\Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
